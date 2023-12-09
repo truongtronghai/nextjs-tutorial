205920919d49a9a95555238a98ba8f2958d551e6
Trong Hai <truongtronghai@gmail.com>
2023-12-09 18:32:19 +0700

    add dashboard page

--- app/page.tsx
+++ app/page.tsx
@@ -14,9 +14,7 @@ export default function Page() {
 			<div className='mt-4 flex grow flex-col gap-4 md:flex-row'>
 				<div className='flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20'>
 					<div className={styles.shape} />
-					<p
-						className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className}`}
-					>
+					<p className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className}`}>
 						<strong>Welcome to Acme.</strong> This is the example for the{' '}
 						<a href='https://nextjs.org/learn/' className='text-blue-500'>
 							Next.js Learn Course
@@ -37,6 +35,13 @@ export default function Page() {
 						width={1000}
 						height={760}
 						className='hidden md:block'
+						alt='Screenshots of the dashboard project showing desktop version'
+					/>
+					<Image
+						src='/hero-mobile.png'
+						width={560}
+						height={620}
+						className='block md:hidden'
 						alt='Screenshots of the dashboard project showing desktop version'
 					/>
 				</div>
